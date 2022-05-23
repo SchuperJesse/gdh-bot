14ddfba9a113e047ab0ad5c9fe8736ef3d736c0d
L33t-HAXXOR <31870368+L33t-HAXXOR@users.noreply.github.com>
2022-05-22 23:45:22 -0500

    Embed Pagination Skeleton

---
+++ src/skills/help/Help_Embed.ts
@@ -0,0 +1,108 @@
+// TYPESCRIPT:
+
+import {
+    Interaction,
+    Message,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed,
+} from 'discord.js'
+import { isReturnStatement } from 'typescript'
+
+const embeds: MessageEmbed[] = []
+const pages = {}  as { [key: string]: number} // {userId: pageNumber}npm i
+
+for (let a = 0; a < 4; ++a) {
+    embeds.push(new MessageEmbed().setDescription(`Page ${a + 1}`))
+}
+
+const getRow = (id: string) => {
+    const row = new MessageActionRow()
+
+    row.addComponents(
+        new MessageButton()
+            .setCustomId('prevEmbed')
+            .setStyle('SECONDARY')
+            .setEmoji('⏮')
+            .setDisabled(pages[id] === 0)
+    )
+    row.addComponents(
+        new MessageButton()
+            .setCustomId('nextEmbed')
+            .setStyle('SECONDARY')
+            .setEmoji('⏭')
+            .setDisabled(pages[id] === embeds.length - 1)
+    )
+
+    return row
+}
+
+export default {
+    category: 'Testing',
+    description: 'Creates an embed pagination',
+
+    slash: 'both',
+    testOnly: true,
+
+    callback: async ({user, message, interaction, channel }) => {
+        const id = user.id
+        pages[id] = pages[id] || 0
+
+        const embed = embeds[pages[id]]
+        let reply: Message | undefined
+        let collector
+
+        const filter = (i: Interaction) => i.user.id === user.id
+        const time = 1000 * 60 * 5
+
+        if(message) {
+            reply = await message.reply({
+                embeds: [embed],
+                components: [getRow(id)],
+            })
+            collector = reply.createMessageComponentCollector({ filter, time})
+        } else {
+            interaction.reply({
+                ephemeral: true,
+                embeds: [embed],
+                components: [getRow(id)],
+            })
+
+            collector = channel.createMessageComponentCollector({filter, time})
+        }
+        collector.on('collect', (btnInt) => {
+            if (!btnInt) {
+                return
+            }
+
+            btnInt.deferUpdate()
+
+            if (
+                btnInt.customId !== 'prevEmbed' &&
+                btnInt.customId !== 'nextEmbed'
+            ) {
+                return
+            }
+
+            if (btnInt.customId === 'prevEmbed' && pages[id] > 0){
+                --pages[id]
+            } else if (
+                btnInt.customId === 'nextEmbed' &&
+                pages[id] < embeds.length - 1
+            ) {
+                ++pages[id]
+            }
+            if (reply) {
+                reply.edit({
+                    embeds: [embeds[pages[id]]],
+                    components: [getRow(id)],
+                })
+            } else {
+                interaction.editReply({
+                    embeds: [embeds[pages[id]]],
+                    components: [getRow(id)]
+                })
+            }
+        })
+    },
+} // as
